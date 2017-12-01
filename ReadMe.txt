ec9a664595743cee2b54446f90f1cd5f4c5889e9
yaodong <nnie@live.cn>
2017-12-01 11:20:41 +0800

    AngularJS分页器组件

---
+++ ReadMe.txt
@@ -0,0 +1,32 @@
+/*********************************************************************************/
+/**                                                                             **/
+/**                         author:   fei                                       **/
+/**                         date:     2017-11-30                                **/
+/**                         describe：基于Bootstrap的一个分页器组件                **/
+/**                         version:  2.0.0                                     **/
+/**                                                                             **/
+/*********************************************************************************/
+
+1、引入Boostrap相关包
+
+2、引入组件
+
+3、在页面引用组件
+        <fei-paging
+                ng-if="rmc.pages.length>0"
+                pager="rmc.pager"
+                page-array="rmc.pages"
+                local-page-no="rmc.pager.pageNum"
+                update-pager="rmc.updatePager($event)"
+                go-page="rmc.search();">
+        </fei-paging>
+
+4、指令参数说明
+        ng-if:         数据有分页时才显示分页组件
+        pager：        分页参数对象，包含页码pageNum、分页大小pageSize以及起始页码startPageNum(只有0或1)三个属性（为啥有这个么奇葩的起始页码startPageNum属性？遇到分页从第0页开始）
+        page-array:    分页页码数组（[1,2,3,4,5...]）
+        local-page-no: 当前页码
+        update-pager:  事件输出：通知下一个查询的分页页码
+        go-page:       事件输出：分页查询函数
+
+5、分页页码达到界限时，通过CSS控制首页、上一页、下一页、尾页的是否点击状态
---
+++ paging.component.js
@@ -0,0 +1,83 @@
+/**
+ * Name:   AngularJS 分页组件
+ * Date:   2017/11/30
+ * Author: fei
+ * Version:2.0.0
+ * @type {{restrict: string, bindings: {paging: string, pageArray: string, localPageNo: string, updatePager: string, goPage: string}, templateUrl: string, controller: feiPaging.controller}}
+ */
+var feiPaging = {
+    restrict: 'E',
+    bindings: {
+        pager: "<",        // 分页参数，包含当前页码pageNum，分页大小pageSize，起始页码startPageNum
+        pageArray: "<",    // 分页页码数组[1,2,3,4,5...]
+        localPageNo: "<",  // 当前分页页码
+        updatePager: "&",  // 分页页码同步事件（事件输出：将下一个要查询的页码返回）
+        goPage: "&"        // 分页查询事件（事件输出：调用实际分页查询函数）
+    },
+    templateUrl: "app/components/pagingComponent/paging.html",     // 分页组件模版Url（这个值，根据实际项目作调整）
+    controller: function () {
+        var fp = this;
+        fp.$onInit = function () {                                 // $onInit生命周期勾子
+            fp.pageSpan = 4;                                       // 分页差值
+            fp.pageMin = fp.pager.startPageNum;                    // 可以显示的分页页码最小值
+            fp.pageMax = fp.pager.startPageNum + fp.pageSpan;      // 可以显示的分页页码最大值
+        };
+        fp.turnPage = function (mark, num) {
+            if ("first"===mark) {// 首页
+                fp.pageMin = fp.pager.startPageNum;
+                fp.pageMax = fp.pager.startPageNum + fp.pageSpan;
+                fp.localPageNo = fp.pager.startPageNum;
+            } else if ("pre"===mark) {// 上一页
+                if (num === fp.pageMin) {
+                    if (fp.pageMin>fp.pageArray[0]) {
+                        fp.pageMin -= 1;
+                        fp.pageMax -= 1;
+                    }
+                }
+                if (fp.pager.startPageNum!=num) {
+                    fp.localPageNo = num - 1;
+                }
+            } else if ("local"===mark) {// 选择具体页码
+                if (num === fp.pageMin) {
+                    if (fp.pageMin>fp.pageArray[0]) {
+                        fp.pageMin -= 1;
+                        fp.pageMax -= 1;
+                    }
+                }
+                if (num === fp.pageMax) {
+                    if (fp.pageMax<fp.pageArray[fp.pageArray.length-1]) {
+                        fp.pageMin += 1;
+                        fp.pageMax += 1;
+                    }
+                }
+                fp.localPageNo = num;
+            } else if ("next"===mark) {// 下一页
+                if (num === fp.pageMax) {
+                    if (fp.pageMax<fp.pageArray[fp.pageArray.length-1]) {
+                        fp.pageMin += 1;
+                        fp.pageMax += 1;
+                    }
+                }
+                if (fp.pager.startPageNum<=num<fp.pageArray[fp.pageArray.length-1]) {
+                    fp.localPageNo = num + 1;
+                }
+            } else if ("last"===mark) {// 尾页
+                fp.pageMin = fp.pageArray[fp.pageArray.length-1] -fp.pageSpan;
+                fp.pageMax = fp.pageArray[fp.pageArray.length-1];
+                fp.localPageNo = fp.pageArray[fp.pageArray.length-1];
+            }
+            fp.updatePager({//调用fp.updatePager函数更新分页页码
+                $event: {
+                    pager: {
+                        startPageNum: fp.pager.startPageNum,
+                        pageNum:      fp.localPageNo,
+                        pageSize:     fp.pager.pageSize
+                    }
+                }
+            });
+            fp.goPage();//调用fp.goPage作分页查询
+        };
+    }
+};
+app.component("feiPaging", feiPaging);
+
